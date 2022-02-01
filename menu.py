2d94df7fae095a01ad8c22c1af0bd3217ac8e64d
vezype <vezype@gmail.com>
2022-02-01 18:04:21 +0300

    Готовый вид меню.

--- menu.py
+++ menu.py
@@ -9,20 +9,6 @@ class Menu(QMainWindow):
         super().__init__()
         uic.loadUi('data/ui/menu.ui', self)
 
-        self.btn_exit.clicked.connect(self.close_app)
-
-        self.btn_arkanoid.clicked.connect(self.open_arkanoid)
-        self.btn_pacman.clicked.connect(self.open_pacman)
-
-    def close_app(self):
-        sys.exit()
-
-    def open_arkanoid(self):
-        pass
-
-    def open_pacman(self):
-        pass
-
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
